b6bc7dfe2296b348b40114e405de1fe47101d650
María José Briceño Nikulin <canetaba>
2023-11-18 23:42:42 +0000

    Initial commit

---
+++ src/main/java/cote/Application.java
@@ -0,0 +1,10 @@
+package cote;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
