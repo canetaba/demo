60a43082d70b1f03e0b20343cf453c8b7a15e348
Maria Jose Briceño Nikulin <mjbriceno@subtel.gob.cl>
2023-11-18 21:34:42 -0300

    cambio de nombre

--- src/test/java/cote/controller/PapuControllerTest.java
+++ src/test/java/cote/controller/PapuControllerTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest
-public class PaupuControllerTest {
+public class PapuControllerTest {
 
 
     @Inject
